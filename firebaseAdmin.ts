fc056e66bb7d7e1edeb2a47c8993add8ca03e99e
Arhan Ansari <arhanansari2009@gmail.com>
2024-07-29 16:09:36 +0000

    2nd try to make vercel app deployable

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,19 +1,18 @@
-import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
+import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-// const serviceKey = require('./service_key.json');
-
-const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+import { getStorage } from "firebase-admin/storage";
 let app: App;
 
-if(getApps().length === 0){
-    app = initializeApp({
-        credential: cert(serviceKey),
-    });
+if (getApps().length === 0) {
+  const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY! as string);
+  app = initializeApp({
+    credential: cert(serviceKey),
+  });
 } else {
-    app = getApp();
+  app = getApp();
 }
 
 const adminDb = getFirestore(app);
+const adminStorage = getStorage(app);
 
-export { adminDb, app as adminApp };
+export { app as adminApp, adminDb, adminStorage };
