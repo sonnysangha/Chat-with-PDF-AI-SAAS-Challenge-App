fa10041b08b49c69c41beb21b07052420725f8a1
Arhan Ansari <arhanansari2009@gmail.com>
2024-08-11 20:25:39 +0000

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
@@ -23,6 +24,7 @@ export default function RootLayout({
           <ConfigForm />
         </body>
       </html>
+      <Analytics/>
     </ClerkProvider>
   );
 }
