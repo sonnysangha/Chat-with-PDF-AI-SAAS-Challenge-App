e2fe3db15882e3d282ab6cbc6800e0a00f96d90a
Arhan Ansari <arhanansari2009@gmail.com>
2024-08-18 16:08:35 +0000

    Fixing VERCEL_URL Problem

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,10 +1,10 @@
 const getBaseUrl = () =>
   process.env.NODE_ENV === "development"
     ? `https://${process.env.NETIFY_URL}`
-    : `https://${process.env.VERCEL_URL}`;
+    : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
   process.env.NODE_ENV === "production"
   ? `https://${process.env.NETIFY_URL}`
-  : `https://${process.env.VERCEL_URL}`;
+  : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
 export default getBaseUrl;
