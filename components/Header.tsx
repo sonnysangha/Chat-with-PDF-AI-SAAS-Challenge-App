7f8bbd750a068e5f2ba71b16b07450943d584bf6
Hein Htoo <heinhtoozaw99@gmail.com>
2024-08-25 17:04:35 +0630

    add flex-wrap

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,13 +11,13 @@ import { dark } from "@clerk/themes";
 function Header() {
   const { theme } = useTheme();
   return (
-    <div className="flex justify-between bg-white shadow-sm p-5 border-b dark:bg-gray-800">
-      <Link href="/dashboard" className="text-2xl">
+    <div className="flex justify-between bg-white shadow-sm p-5 border-b dark:bg-gray-800 gap-5">
+      <Link href="/dashboard" className="text-2xl whitespace-nowrap">
         Chat to <span className="text-indigo-600">PDF</span>
       </Link>
 
       <SignedIn>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center flex-wrap justify-center gap-3">
           <Button asChild variant="link" className="hidden md:flex">
             <Link href="/dashboard/upgrade">Pricing</Link>
           </Button>
